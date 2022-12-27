da70ea8fd08473ca3377615b5ac9d9d51c119b65
ovitorsaf <vitoraffemp@gmail.com>
2022-12-26 21:29:48 -0300

    feat: add useRef on Login page

--- src/app/pages/login/Login.tsx
+++ src/app/pages/login/Login.tsx
@@ -1,26 +1,24 @@
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 
 export const Login = () => {
+    
+    const inputPassRef = useRef<HTMLInputElement>(null);
     
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 
     const emailLength = useMemo(() => {
-        console.log('executou')
         return email.length * 1000
     }, [email.length]);
 
     useEffect(() => {
         console.log(email);  
-    }, [email]);
-
-    useEffect(() => {
         console.log(pass);  
-    }, [pass]);
+    }, [email, pass]);
 
-    const handleEntrar = () => {
+    const handleEntrar = useCallback(() => {
         console.log(email, ' | ', pass);
-    }
+    }, [email, pass]);
 
     return(
         <div>
@@ -30,12 +28,22 @@ export const Login = () => {
 
                 <label>
                     <span>Email</span>
-                    <input type="text" value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input 
+                        type="text" 
+                        value={email} 
+                        onChange={e => setEmail(e.target.value)}
+                        onKeyDown={e => e.key === 'Enter' ? inputPassRef.current?.focus() : undefined}
+                    />
                 </label>
 
                 <label>
                     <span>Senha</span>
-                    <input type="password" value={pass} onChange={e => setPass(e.target.value)} />
+                    <input 
+                        type="password" 
+                        value={pass} 
+                        ref={inputPassRef}
+                        onChange={e => setPass(e.target.value)}
+                    />
                 </label>
 
                 <button type="button" onClick={handleEntrar}>Entrar</button>
