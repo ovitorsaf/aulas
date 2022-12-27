d5b7eda392d76a83a23fc149d6b2930fc7796484
ovitorsaf <vitoraffemp@gmail.com>
2022-12-26 21:37:52 -0300

    feat: add useRef on Dashboard Page

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,22 @@
+import { useRef } from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
 
+    const counterRef = useRef({ counter: 0 });
+
     return(
         <div>
+            
             <h1>Home | Dashboard</h1>
+            
+            <p>Contador: {counterRef.current.counter}</p>
+            
+            <button onClick={() => counterRef.current.counter++}>Somar</button>
+            <button onClick={() => console.log(counterRef.current.counter)}>Log</button>
+            <br/>
             <Link to="/entrar">Login</Link>
+
         </div>
     );
 
