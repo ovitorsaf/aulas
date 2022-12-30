5d2490f1ecf5904ac6b98449620f5be9abc07bd2
ovitorsaf <vitoraffemp@gmail.com>
2022-12-30 03:18:56 -0300

    add: crud com backend

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -1,37 +1,46 @@
-import { useCallback, useState } from 'react';
-
-interface ITarefa {
-    id: number;
-    title: string;
-    isCompleted: boolean;
-}
+import { useCallback, useEffect, useState } from 'react';
+import { ApiException } from '../../shared/services/api/ApiException';
+import { ITarefa, TarefasService } from '../../shared/services/api/tarefas/TarefasService';
 
 export const Dashboard = () => {
 
     const [lista, setLista] = useState<ITarefa[]>([]);
 
-    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
-        if(e.key === 'Enter'){
-            if(e.currentTarget.value.trim().length === 0) return;
-
-            const value = e.currentTarget.value;
-
-            setLista((oldLista) => {
-                if(oldLista.some((listItem) => listItem.title === value)) return oldLista;
+    useEffect(() => {
+        TarefasService.getAll()
+            .then((result) => {
+                if (result instanceof ApiException){
+                    alert(result.message);
+                } else {
+                    setLista(result);
+                }
                 
-                return [
-                    ...oldLista, 
-                    {
-                        title: value,
-                        isCompleted: false,
-                        id: oldLista.length
-                    }
-                ];
             });
-
-            e.currentTarget.value = '';
-        }
     }, []);
+
+    const handleInputKeyDown: React.KeyboardEventHandler<HTMLInputElement> = useCallback((e) => {
+        if (e.key === 'Enter') {
+          if (e.currentTarget.value.trim().length === 0) return;
+    
+          const value = e.currentTarget.value;
+    
+          e.currentTarget.value = '';
+    
+          if (lista.some((listItem) => listItem.title === value)) return;
+    
+          TarefasService.create({ title: value, isCompleted: false })
+            .then((result) => {
+              if (result instanceof ApiException) {
+                alert(result.message);
+              } else {
+                /* Esta com erro porém ainda executa a criação no banco... Verificando no Discord o que pode ser */
+                setLista((oldLista) => {
+                    return [...oldLista, result];
+                });
+              }
+            });
+        }
+      }, [lista])
 
 
     return(
